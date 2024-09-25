0f2a707dad168a1c09409f5310341c933f19fd34
Ubuntu <ubuntu@ip-172-31-16-251.us-east-2.compute.internal>
2024-09-25 21:26:47 +0000

    made new chnages to my java files

---
+++ javascript/webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  resolve: {
+    fallback: {
+      "buffer": require.resolve("buffer/"),
+      "http": require.resolve("stream-http"),
+      "https": require.resolve("https-browserify"),
+      "url": require.resolve("url/")
+    }
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      Buffer: ['buffer', 'Buffer'],
+    }),
+  ],
+  mode: 'development'
+};
